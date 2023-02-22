2831fbc78191af4e5b1c18436b6b2dead1f507ac
tom-stephen <tom.stephen@ucalgary.ca>
2023-02-21 17:14:18 -0700

    adding some content to work on the main page

--- app.py
+++ app.py
@@ -296,7 +296,7 @@ if __name__ == "__main__":
     user = 'tom'
     password = 'password'
     cnx = mysql.connector.connect(
-    host ="127.0.0.1",
+    host ="localhost",
     port = 3306,
     user = user,
     password = password)
